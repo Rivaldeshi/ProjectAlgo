ddba0de1cfe9229229567de42c1d0ef695dc5c14
RIVALDES Ranol <tiyorivaldes@gmail.com>
2021-12-08 07:24:55 +0100

    fi

--- src/Vigenere/Vigenere.java
+++ src/Vigenere/Vigenere.java
@@ -2,23 +2,28 @@ package Vigenere;
 
 public class Vigenere {
 
+	static String Alphabet = "abcdefghijklmnopqrstuvwxyz";
+
 	// fontion de criptage
 	public static String Crypter(String text, String cle) {
-      
+
 		// recupere la cle ayant la meme longuer que le mot
 		cle = getLaLongueCle(text, cle);
 
 		String textCrypter = "";
-	   
-	    // on parcour le mot de on cherche la version cripter de chaque letre schant que la cle et le text son de meme taille
+
+		// on parcour le mot de on cherche la version cripter de chaque letre
+		// schant que la cle et le text son de meme taille
 		for (int i = 0; i < text.length(); i++) {
-			char key = cle.charAt(i);
-			char value = text.charAt(i);
-            // calcule de caractere cryter
-			int x = (value - 'a' + key - 'a') % 26;
-			x += 'a';
+
+			int key = Alphabet.indexOf(cle.charAt(i));
+			int value = Alphabet.indexOf(text.charAt(i));
+
+			// calcule de caractere cryter
+			int x = (value + key) % 26;
+
 			// ajoute a la chaine des crypter
-			textCrypter += (char) (x);
+			textCrypter += Alphabet.charAt(x);
 		}
 		return textCrypter;
 	}
@@ -26,18 +31,18 @@ public class Vigenere {
 	public static String DecripterText(String text, String cle) {
 
 		// exactement le meme procesus
-		
+
 		cle = getLaLongueCle(text, cle);
 		String textDecripte = "";
 
 		for (int i = 0; i < text.length(); i++) {
 
-			char key = cle.charAt(i);
-			char value = text.charAt(i);
+			int key = Alphabet.indexOf(cle.charAt(i));
+			int value = Alphabet.indexOf(text.charAt(i));
 
 			int x = (value - key + 26) % 26;
-		    x += 'a';
-			textDecripte += (char) (x);
+
+			textDecripte += Alphabet.charAt(x);
 		}
 		return textDecripte;
 	}
@@ -46,12 +51,10 @@ public class Vigenere {
 	// sera juste un repetition de la cle normal juska la taille du text
 	public static String getLaLongueCle(String text, String cle) {
 		String longueCle = "";
-		
 		for (int i = 0; i < text.length(); i++) {
 			longueCle += cle.charAt(i % cle.length());
 		}
 		return longueCle;
-
 	}
 
 }
